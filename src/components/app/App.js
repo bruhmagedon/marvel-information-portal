4b7ac7519c980deed64e79da3cc19682b9c8834c
bruhmagedon <99552300+bruhmagedon@users.noreply.github.com>
2023-12-16 08:16:23 +0300

    idk

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,25 +2,26 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
 import { MainPage, ComicsPage, Page404, SingleComicPage } from "../pages/index";
+
 const App = () => {
-    return (
-        <Router>
-            <div className="app">
-                <AppHeader />
-                <Routes>
-                    <Route exact path="/" element={<MainPage />} />
-                    <Route exact path="/comics" element={<ComicsPage />} />
-                    <Route
-                        exact
-                        //отдельный комикс по определенному айди
-                        path="/comics/:comicId"
-                        element={<SingleComicPage />}
-                    />
-                    <Route exact path="*" element={<Page404 />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <Routes>
+          <Route exact path="/" element={<MainPage />} />
+          <Route exact path="/comics" element={<ComicsPage />} />
+          <Route
+            exact
+            //отдельный комикс по определенному айди
+            path="/comics/:comicId"
+          />
+          {/* если не загрузился ни один из роутов */}
+          <Route exact path="*" element={<Page404 />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 // че можно сделать по проекту
--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -9,92 +9,92 @@ import Spinner from "../spinner/Spinner";
 import "./charInfo.scss";
 
 const CharInfo = (props) => {
-    const [char, setChar] = useState(null);
+  const [char, setChar] = useState(null);
 
-    const { loading, error, getCharacter, clearError } = useCharMarvelService();
+  const { loading, error, getCharacter, clearError } = useCharMarvelService();
 
-    useEffect(() => {
-        updateChar();
-        // eslint-disable-next-line
-    }, [props.charId]);
+  useEffect(() => {
+    updateChar();
+    // eslint-disable-next-line
+  }, [props.charId]);
 
-    const updateChar = () => {
-        const { charId } = props;
-        if (!charId) return;
+  const updateChar = () => {
+    const { charId } = props;
+    if (!charId) return;
 
-        clearError();
-        getCharacter(charId).then(onCharLoaded);
-    };
+    clearError();
+    getCharacter(charId).then(onCharLoaded);
+  };
 
-    const onCharLoaded = (char) => {
-        setChar(char);
-    };
+  const onCharLoaded = (char) => {
+    setChar(char);
+  };
 
-    const skeleton = char || loading || error ? null : <Skeleton />;
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error || !char) ? <View char={char} /> : null;
+  const skeleton = char || loading || error ? null : <Skeleton />;
+  const errorMessage = error ? <ErrorMessage /> : null;
+  const spinner = loading ? <Spinner /> : null;
+  const content = !(loading || error || !char) ? <View char={char} /> : null;
 
-    return (
-        <div className="char__info">
-            {skeleton}
-            {errorMessage}
-            {spinner}
-            {content}
-        </div>
-    );
+  return (
+    <div className="char__info">
+      {skeleton}
+      {errorMessage}
+      {spinner}
+      {content}
+    </div>
+  );
 };
 
 const View = ({ char }) => {
-    const { name, description, thumbnail, homepage, wiki, comics } = char;
+  const { name, description, thumbnail, homepage, wiki, comics } = char;
 
-    let imgStyle = { objectFit: "cover" };
-    if (
-        thumbnail ===
-        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
-    ) {
-        imgStyle = { objectFit: "contain" };
-    }
+  let imgStyle = { objectFit: "cover" };
+  if (
+    thumbnail ===
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+  ) {
+    imgStyle = { objectFit: "contain" };
+  }
 
-    return (
-        <>
-            <div className="char__basics">
-                <img src={thumbnail} alt="abyss" style={imgStyle} />
-                <div>
-                    <div className="char__info-name">{name}</div>
-                    <div className="char__btns">
-                        <a href={homepage} className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href={wiki} className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div className="char__descr">
-                {description === "" ? "No data about character" : description}
-            </div>
-            <div className="char__comics">Comics:</div>
-            <ul className="char__comics-list">
-                {comics.length
-                    ? comics.map((item, i) => {
-                          // eslint-disable-next-line
-                          if (i > 10) return;
-                          return (
-                              <li className="char__comics-item" key={i}>
-                                  {item.name}
-                              </li>
-                          );
-                      })
-                    : "No comics"}
-            </ul>
-        </>
-    );
+  return (
+    <>
+      <div className="char__basics">
+        <img src={thumbnail} alt="abyss" style={imgStyle} />
+        <div>
+          <div className="char__info-name">{name}</div>
+          <div className="char__btns">
+            <a href={homepage} className="button button__main">
+              <div className="inner">homepage</div>
+            </a>
+            <a href={wiki} className="button button__secondary">
+              <div className="inner">Wiki</div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="char__descr">
+        {description === "" ? "No data about character" : description}
+      </div>
+      <div className="char__comics">Comics:</div>
+      <ul className="char__comics-list">
+        {comics.length
+          ? comics.map((item, i) => {
+              // eslint-disable-next-line
+              if (i > 10) return;
+              return (
+                <li className="char__comics-item" key={i}>
+                  {item.name}
+                </li>
+              );
+            })
+          : "No comics"}
+      </ul>
+    </>
+  );
 };
 
 CharInfo.propTypes = {
-    charId: PropTypes.number,
+  charId: PropTypes.number,
 };
 
 export default CharInfo;
